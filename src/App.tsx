919f94779fa2c27c3fa37c87f77da8d91648e5ba
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-11-20 18:56:03 +0100

    Add grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
+import MemoryGame from "./components/memory-game";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <MemoryGame />;
 }
